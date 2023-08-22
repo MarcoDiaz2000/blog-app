bb329c8c8818cb5bd7349ea929885ebb7955d284
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-08-22 08:22:08 -0600

    Add test for User and Post show controllers

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
+RSpec.describe "Users", type: :request do
+  describe "GET /index" do
     it 'returns a success response' do
       get users_path
       expect(response).to have_http_status(:success)
-      expect(response.body).to include('This is a list of users')
+      expect(response.body).to include("This is a list of users")
+    end
+  end
+  
+  describe "GET /show" do
+    it 'returns a success response' do
+      user = User.create!(name: 'Name', posts_counter: 0)
+      get user_path(user)
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include("Here is detail of a specific user")
     end
   end
 end

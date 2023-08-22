8cd5b534e395a75a0ea5dfd38af8ffc115b9a845
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-08-22 08:22:51 -0600

    Fix linters

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -10,13 +10,13 @@ RSpec.describe 'Posts', type: :request do
     end
   end
 
-    describe 'GET /show' do
-      it 'returns a success response' do
-        user = User.create!(name: 'Name', posts_counter: 0)
-        post = Post.create!(title: 'Post Title', author_id: user.id, comments_counter: 0, likes_counter: 0)
-        get user_post_path(user_id: user.id, id: post.id)
-        expect(response).to have_http_status(:success)
-        expect(response.body).to include('Here is detail of a post for a specific user')
-      end
+  describe 'GET /show' do
+    it 'returns a success response' do
+      user = User.create!(name: 'Name', posts_counter: 0)
+      post = Post.create!(title: 'Post Title', author_id: user.id, comments_counter: 0, likes_counter: 0)
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('Here is detail of a post for a specific user')
     end
+  end
 end

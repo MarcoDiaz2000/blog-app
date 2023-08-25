9f6ef6d5c77b8db3f7a48b26c07f14e35c76430f
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-08-25 03:56:22 -0600

    Fix the creation of new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
 
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  private
+
+  def set_default_posts_counter
+    self.posts_counter ||= 0
   end
 end

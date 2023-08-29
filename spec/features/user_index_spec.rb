e1089873df6f2b30775194834eab9bc8b1d4047e
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-08-29 00:40:32 -0600

    add user show spec

---
+++ spec/features/user_index_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.feature "User index page", type: :feature do
+  before do
+    @user1 = User.create(name: "Marco", photo: 'https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg', bio: 'Text for Bio', posts_counter: 0)
+
+    3.times do |i|
+      Post.create(title: "Post #{i+1}", author: @user1, comments_counter: 0, likes_counter: 0)
+    end
+
+    @user2 = User.create(name: "Tumaini", photo: 'https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg', bio: 'Text for Bio', posts_counter: 0)
+  end
+  
+
+  scenario "I can see the username of all other users" do
+    visit users_path
+
+    expect(page).to have_content("Marco")
+    expect(page).to have_content("Tumaini")
+  end
+
+  scenario "I can see the profile picture for each user" do
+    visit users_path
+
+    expect(page).to have_css("img[src*='#{@user1.photo}']")
+    expect(page).to have_css("img[src*='#{@user2.photo}']")
+  end
+
+  scenario "I can see the number of posts each user has written" do
+    visit users_path
+
+    expect(page).to have_content("Number of posts: 3")
+    expect(page).to have_content("Number of posts: 0")
+  end
+
+  scenario "When I click on a user, I am redirected to that user's show page" do
+    visit users_path
+    find(".user-row-link", text: "Marco").click
+
+    expect(page.current_path).to eq(user_path(@user1))
+  end
+end
---
+++ spec/features/user_show_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.feature "User show page", type: :feature do
+  before do
+    @user = User.create(name: "Marco", photo: 'https://...', bio: 'Text for Bio', posts_counter: 0)
+  
+    5.times do |i|
+      Post.create(title: "Post #{i+1}", author: @user, comments_counter: 0, likes_counter: 0)
+    end
+  end
+
+  scenario "I can see the user's profile picture" do
+    visit user_path(@user)
+
+    expect(page).to have_css("img[src*='#{@user.photo}']")
+  end
+
+  scenario "I can see the user's username" do
+    visit user_path(@user)
+
+    expect(page).to have_content(@user.name)
+  end
+
+  scenario "I can see the number of posts the user has written" do
+    visit user_path(@user)
+
+    expect(page).to have_content("Number of posts: 5")
+  end
+
+  scenario "I can see the user's bio" do
+    visit user_path(@user)
+
+    expect(page).to have_content(@user.bio)
+  end
+
+  scenario "I can see the user's first 3 posts" do
+    visit user_path(@user)
+
+    @user.three_most_recent_posts.each do |post|
+      expect(page).to have_content(post.title)
+    end
+  end
+
+  scenario "I can see a button that lets me view all of a user's posts" do
+    visit user_path(@user)
+
+    expect(page).to have_link("See all posts")
+  end
+
+  scenario "When I click a user's post, it redirects me to that post's show page" do
+    visit user_path(@user)
+    first_post_title = @user.three_most_recent_posts.first.title
+    click_link(first_post_title)
+    expect(page).to have_content(first_post_title)
+  end
+
+  
+
+  scenario "When I click to see all posts, it redirects me to the user's post's index page" do
+    visit user_path(@user)
+    click_link("See all posts")
+
+    expect(page.current_path).to eq(user_posts_path(@user))
+  end
+end

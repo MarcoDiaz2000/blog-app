3e41e2d669bb9099ce300910f0dc1c9ee7ddb501
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-08-30 17:51:28 -0600

    Fix error with route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def after_inactive_sign_up_path_for(resource)
-    new_user_session_path
-  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
+
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do

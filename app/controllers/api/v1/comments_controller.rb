0100679dbff7b1026e97ec0ca0e512640956adbf
Tumaini Maganiko <tumainimaganiko@gmail.com>
2023-09-01 09:59:54 +0300

    Create create Method

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -3,4 +3,16 @@ def index
     post = Post.find(params[:post_id])
     render json: post.comments
   end
+ def create
+    user = User.find_by(email: params[:email])
+    logger.debug "User found: #{user.inspect}"
+    if user&.authenticate(params[:password])
+      logger.debug "User authenticated"
+      token = JwtManager.encode(user_id: user.id)
+      render json: { auth_token: token }
+    else
+      logger.debug "Invalid credentials"
+      render json: { error: 'Invalid credentials' }, status: 401
+    end
+  end
 end
